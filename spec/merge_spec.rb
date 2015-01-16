0baecf783ae99b1e2ad671d7e05936f4c05a510c
bootstraponline <code@bootstraponline.com>
2015-01-16 16:06:42 -0500

    Refactor merge test

--- spec/merge_spec.rb
+++ spec/merge_spec.rb
@@ -1,8 +1,7 @@
 describe 'merge rspec_junit xml' do
   it 'should merge successfully' do
-    actual   = RSpecFlake.merge_xml input: [data('0.xml'), data('1.xml'),
-                                            data('2.xml'), data('failure.xml'),
-                                            data('failure2.xml')]
+    xml_files = %w[0.xml 1.xml 2.xml failure.xml failure2.xml]
+    actual   = RSpecFlake.merge_xml input: xml_files.map(&method(:data))
     expected = expected_merge_xml
 
     expect(actual).to eq expected
